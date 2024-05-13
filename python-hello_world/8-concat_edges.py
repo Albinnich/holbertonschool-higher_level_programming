5d950e9384c2ef985aa05d716d305366e77785c8
Albinnich <albina_haji@hotmail.com>
2024-05-13 06:19:19 +0000

    Code edited

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str2 = "object-oriented programming with Python"
-print(str2)
+print(str[str.find("object-oriented"):str.find("object-oriented") + len("object-oriented")] +
+        " with " + str[str.find("Python"):], end='\n')
