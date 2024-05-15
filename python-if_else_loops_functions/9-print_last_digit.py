68f2ef36334336d0c8f1e8a1a5d01211c98073b3
Albinnich <albina_haji@hotmail.com>
2024-05-15 15:59:28 +0000

    End added

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    number = abs(number)
     last_digit = number % 10
-    print(last_digit)
+    print(last_digit, end="")
     return last_digit
