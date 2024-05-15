5f52c7961953889aa2c05258c96bcd38f934f20a
Albinnich <albina_haji@hotmail.com>
2024-05-15 10:34:49 +0000

    Pyco style edited

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for k in range(i+1, 10):
-        print("{:d}{:d}".format(i, k), end=', ' \
-                if i != 8 or k != 9 else "\n")
+        print("{:d}{:d}".format(i, k), end=', ' if i != 8 or \
+                k != 9 else "\n")
