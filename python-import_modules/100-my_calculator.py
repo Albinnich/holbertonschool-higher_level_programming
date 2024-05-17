a4691503cba93b6fb6da020ef20eada62eedb4cd
Albinnich <albina_haji@hotmail.com>
2024-05-17 12:10:28 +0000

    Error fixed

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -6,9 +6,11 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     operator = sys.argv[2]
+
     if operator == '+':
         result = add(a, b)
     elif operator == '-':
