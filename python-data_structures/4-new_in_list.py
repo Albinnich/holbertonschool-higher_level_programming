8824d46f8361c222352b38dc2eeca123d317214a
Albinnich <albina_haji@hotmail.com>
2024-05-20 12:00:57 +0000

    Code added

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
+    copied_list = my_list.copy()
     if idx < 0 or idx >= len(my_list):
-        return my_list
+        return my_list.copy()
     else:
         my_list[idx] = element
         return my_list
