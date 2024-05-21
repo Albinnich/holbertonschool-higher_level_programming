4064c7510e2586d5849d634ef9e896c7955f1423
Albinnich <albina_haji@hotmail.com>
2024-05-21 08:45:10 +0000

    Code fixed

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    print("{:d}{:d}{d}".format(matrix))
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}{:d}".format(i, j), end=' ')
+        print()
