e7d4dc8541bc9ef18b2b0fec5c779097f61ff66a
Albinnich <albina_haji@hotmail.com>
2024-05-21 07:33:27 +0000

    Code fixed

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    character_to_remove = "c"
-    translation_table = str.maketrans("", character_to_remove)
-    new_string = text.translate(translation.table)
-    print(new_string)
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
