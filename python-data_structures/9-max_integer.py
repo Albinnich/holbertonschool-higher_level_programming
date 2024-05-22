3808496b640e9de9d503de0fba40a84db935b2b6
Albinnich <albina_haji@hotmail.com>
2024-05-22 06:27:10 +0000

    Code edited

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    largest = my_list
-    for i in range(1, len(my_list)):
-        if my_list[i] > largest:
-            largest = my_list[i]
-    return largest
+    my_list.sort()
+    return my_list[-1]
