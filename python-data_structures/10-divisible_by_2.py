ff1c4999a55964edf28d9d5828a653c6bdec2606
Albinnich <albina_haji@hotmail.com>
2024-05-22 06:45:56 +0000

    Code edited

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 
 def divisible_by_2(my_list=[]):
     for i in my_list:
-        if i % 2 == 0:
-            return True
-        else:
+        if i%2 != 0:
             return False
+        else:
+            return True
