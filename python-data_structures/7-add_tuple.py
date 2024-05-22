feec7ec8f688d324c986a049b088e20666e3e0ad
Albinnich <albina_haji@hotmail.com>
2024-05-22 05:09:44 +0000

    Code edited

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    res=[]
-    for i in range(0,len(tuple_a)):
-        res.append(tuple_a[i]+tuple_b[i])
-    res=tuple(res)
-    print(tuple)
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result
