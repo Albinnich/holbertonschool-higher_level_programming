1121841d80de483448c4cd8f04dbfd835152769a
Albinnich <albina_haji@hotmail.com>
2024-05-22 05:53:40 +0000

    Code fixed for 0

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,4 +4,7 @@ def multiple_returns(sentence):
     tuplex = tuple(sentence)
     length = len(tuplex)
     first = tuplex[0]
-    print(f"Length: {length} - First character: {first}")
+    if length == 0:
+        print("Length: 0 - First character: None")
+    else:
+        print(f"Length: {length} - First character: {first}")
