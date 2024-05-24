8b26b1defdc64d1a2cf3d7905562866567044ab5
Albinnich <albina_haji@hotmail.com>
2024-05-24 07:36:15 +0000

    Code edited

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    set_3 = set_1.intersection(set_2)
-    print(set_3)
+    first_set = set(set_1)
+    second_set = set(set_2)
+
+    if (first_set & second_set):
+        print(first_set & second_set)
