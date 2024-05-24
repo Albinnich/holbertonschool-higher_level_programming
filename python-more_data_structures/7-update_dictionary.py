25f62b58f8aca38e7dfe00d0f2a38c564c0585db
Albinnich <albina_haji@hotmail.com>
2024-05-24 09:28:45 +0000

    Code fixed

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update(key)
-    a_dictionary.update(value)
-    print(a_dictionary)
+    a_dictionary[key] = value
+    return a_dictionary
