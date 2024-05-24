bab7d02d4d1365480fc630fcc7b7628833045738
Albinnich <albina_haji@hotmail.com>
2024-05-24 17:18:18 +0000

    Code changed

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if key in a_dictionary:
-        return maxscore = max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
-    else:
+    if not in a_dictionary:
         return None
+    return max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
