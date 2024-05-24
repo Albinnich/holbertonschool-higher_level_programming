62a39a61d4f272f7ca7a9d2b196d2a98b45b9ab8
Albinnich <albina_haji@hotmail.com>
2024-05-24 07:42:04 +0000

    Return instead of print added

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    first_set = set(set_1)
-    second_set = set(set_2)
-
-    if (first_set & second_set):
-        print(first_set & second_set)
+    return set_1 & set_2
