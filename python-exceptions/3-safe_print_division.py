e5ccd205adf813ed07b3fa3c877cfc10c52f5176
Albinnich <albina_haji@hotmail.com>
2024-05-25 12:30:03 +0000

    Code fixed

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,8 +3,7 @@
 def safe_print_division(a, b):
     result = a / b
     try:
-        print("Inside result: " + result)
-        print("{:d}".format(result))
+        print("Inside result: {}".format(result))
         return True
     except Exception:
         return False
