23b084747bfd357ba680d69b9fac9db3772da25e
Albinnich <albina_haji@hotmail.com>
2024-05-25 13:40:06 +0000

    finally added

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -25,4 +25,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             results.append(0)
+        finally:
+            pass
     return results
