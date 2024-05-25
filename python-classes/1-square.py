57bd5f3ad6725d48fb2b6c1c5f157278aab6eaf1
Albinnich <albina_haji@hotmail.com>
2024-05-25 16:42:19 +0000

    Code added

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This module defines a class Square that defines a square.
+"""
+
+
+class Square:
+    """
+    This is a square class based on 0-square.py
+    """
+    
+    def _init_(self, size):
+        """
+        Starts a new Square instance.
+
+        Args:
+        size(int): The size of the square.
+        """
+        self._size = size
