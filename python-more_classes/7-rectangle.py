043f02a78412dbb2066e1c93f24f53bf71ce623d
Albinnich <albina_haji@hotmail.com>
2024-05-29 11:47:08 +0000

    code fixed

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -13,7 +13,6 @@ class Rectangle:
     """
 
     number_of_instances = 0
-
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
@@ -23,6 +22,7 @@ class Rectangle:
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
+        self.print_symbol = Rectangle.print_symbol
 
     @property
     def width(self):
