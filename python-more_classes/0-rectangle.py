e5c4125a572add3744663f69de91f73270f1d907
Albinnich <albina_haji@hotmail.com>
2024-05-29 06:04:49 +0000

    Code added

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
 class Rectangle():
-    def __init__(self,breadth,length):
-        self.breadth=breadth
-        self.length=length
+
+    def __init__(self, breadth, length):
+        self.breadth = breadth
+        self.length = length
+
     def area(self):
         return self.breadth*self.length
+
+
 print()
