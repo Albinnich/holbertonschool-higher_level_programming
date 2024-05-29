84ecc079de2a970a308d73812cae916a6562e86f
Albinnich <albina_haji@hotmail.com>
2024-05-29 11:57:07 +0000

    Code fixed

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -62,23 +62,38 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """
+        Returns the area of the rectangle
+        """
         return self.__width * self.__height
 
     def perimeter(self):
+        """
+        Returns the perimeter of the rectangle
+        """
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
+        """
+        Returns the string representation of the rectangle
+        """
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(
-            [str(Rectangle.print_symbol) * self.__width
+            [str(self.print_symbol) * self.__width
                 for _ in range(self.__height)])
 
     def __repr__(self):
+        """
+        Returns a string representation of the rectangle for recreation
+        """
         return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
+        """
+        Prints a message when the instance is deleted
+        """
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
