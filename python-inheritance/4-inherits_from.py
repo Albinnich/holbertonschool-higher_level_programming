f509f40368f1f6b2904194f5dc5c5e96e37d1813
Albinnich <albina_haji@hotmail.com>
2024-05-31 12:11:51 +0000

    Code edited

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -12,9 +12,7 @@ def inherits_from(obj, a_class):
         (directly or indirectly) from the specified class
     """
 
-    if isinstance(obj, a_class):
-        return True
-    elif not type(obj) is a_class:
+    if isinstance(obj, a_class) and not type(obj) is a_class:
         return True
     else:
         return False
