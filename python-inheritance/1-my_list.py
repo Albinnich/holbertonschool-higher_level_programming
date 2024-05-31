b0dbbeeea75c73a84423707eeeb07e060417bc4b
Albinnich <albina_haji@hotmail.com>
2024-05-31 09:54:32 +0000

    Code edited

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,11 +8,14 @@ This module writes a class Mylist that inherits from list
 class MyList(list):
     """
     A subclass of list that can print itself sorted
+
+    Methods:
+        print_sorted(self): Prints list in ascending sorted order
     """
 
 
     def print_sorted(self):
         """
-        Prints the list but sorted
+        Prints the list but sorted in ascending order
         """
         print(sorted(self))
