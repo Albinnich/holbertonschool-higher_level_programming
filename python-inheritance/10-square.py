648f4b0b7c0b9a129d91ce3eb8dbcba6f8a2a485
Albinnich <albina_haji@hotmail.com>
2024-06-01 12:56:39 +0000

    Code fixed

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,47 +1,18 @@
 #!/usr/bin/python3
 
 """
-    The ''10. Square #1'' module
+    The ``Geometry`` module.
 """
+
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """
-    Class that inherits from Rectangle
+        Square class that inherits from Rectangle
     """
-
     def __init__(self, size):
-        """
-        Initializes size
-        """
-
+        super().integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
-
-    def __str__(self):
-         """Return the square description."""
-         return "[Square] {}/{}".format/
-     (self._Rectangle__width, self._Rectangle__height)
-
-    def area(self):
-        """
-        Defines area
-        """
-        return self._Rectangle__width * self._Rectangle__height
-
-if __name__ == "__main__":
-    s = Square(5)
-
-    print(s)
-    print(dir(s))
-
-    try:
-        print("Square: {}".format(s.size))
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        s2 = Square(0)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
