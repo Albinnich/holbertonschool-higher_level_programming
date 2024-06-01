fb9ad05c59ed0ba00a34f6a5fa4372c3d2d0f2f5
Albinnich <albina_haji@hotmail.com>
2024-06-01 08:57:30 +0000

    Code added

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+    The ''6. Improve Geometry'' module
+"""
+
+
+class BaseGeometry:
+    """
+    Class that defines area
+    """
+
+    def area(self):
+        """
+        Defines area with exception
+        """
+        raise Exception("area() is not implemented")
