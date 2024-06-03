e2f01dba1770ce120b987f7ec536798a3abb1b4f
Albinnich <albina_haji@hotmail.com>
2024-06-03 06:48:37 +0000

    Module is documented

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
+"""
+    The ''0. Read file'' module
+"""
+
 def read_file(filename=""):
+    """
+    read_file function reads text file
+    """
+
     with open('UTF8.txt') as f:
         UTF8 = f.read()
         print(UTF8)
