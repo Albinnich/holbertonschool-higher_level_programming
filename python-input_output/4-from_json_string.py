6f9508b33d497e691dfef4be3641a788cb5ca5f9
Albinnich <albina_haji@hotmail.com>
2024-06-03 11:52:10 +0000

    Code fixed

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -11,5 +11,5 @@ def from_json_string(my_str):
     """
     from_json_string function returns object represented by JSON
     """
-    obj = json.loads(my_str):
-        return obj
+    obj = json.loads(my_str)
+    return obj
