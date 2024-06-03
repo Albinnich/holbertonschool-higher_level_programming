0a26b600e647b2fd38376dfbbeb777063e7ce889
Albinnich <albina_haji@hotmail.com>
2024-06-03 11:20:11 +0000

    Code added

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+    The ''4. From JSON string to Object'' module
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    from_json_string function returns object represented by JSON
+    """
+    return json_load(my_str)
