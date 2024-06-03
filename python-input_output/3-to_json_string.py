2b644106780d0fc873e75215af3a366c90cd863f
Albinnich <albina_haji@hotmail.com>
2024-06-03 09:38:06 +0000

    Code added

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+    The ''To JSON string" module
+"""
+
+import json
+
+def to_json_string(my_obj):
+    """
+    to_json_string function returns JSON representation of string
+    """
+
+    my_obj = json.loads(to_json_string)
