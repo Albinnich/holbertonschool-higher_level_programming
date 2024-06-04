1d574040e0f4a3fd71933c2e6ece32ac2b06baae
Albinnich <albina_haji@hotmail.com>
2024-06-04 12:35:33 +0000

    Code edited

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -6,16 +6,20 @@ The ''9. Student to JSON'' module
 
 
 class Student:
+    """
+    Student class
+    """
     def __init__(self, first_name, last_name, age):
         """
         Initializes with first_name, last_name and age
         """
-        def to_json(self):
-            """
-            Retrieves a dictionary representation of the Student instance
-            """
-            return {
-                'first_name': self.first_name,
-                'last_name': self.last_name,
-                'age': self.age
-            }
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of the Student instance
+        """
+        return self.__dict__
