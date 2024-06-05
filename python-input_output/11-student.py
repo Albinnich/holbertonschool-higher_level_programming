122ab2ae718a643fc5e7a6e724ef2eee560cbb03
Albinnich <albina_haji@hotmail.com>
2024-06-05 08:33:14 +0000

    Code added

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+The ''11. Student to disk and reload'' module
+"""
+
+import os
+import json
+
+
+class Student:
+    """
+    Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes with first_name, last_name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of the Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs
+                    if hasattr(self, attr)}
