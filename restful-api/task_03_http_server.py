890deb1b477dc7e0fb07919748506a422099e7da
Albinnich <albina_haji@hotmail.com>
2024-06-07 09:41:04 +0000

    Code edited

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,12 +1,35 @@
 import http.server
 import socketserver
+import json
 
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write("Hello, this is your API response!")
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is your API response!")
+
+        elif self.path == '/data':
+            data = {"name": "John", "age": 30, "city": "London"}
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == '/info':
+            info = {"version": "1.0", "description": "A simple API
+                built with http.server"}
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(info).encode())
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
 
 PORT = 8000
 with socketserver.TCPServer(("", PORT), MyRequestHandler) as httpd:
