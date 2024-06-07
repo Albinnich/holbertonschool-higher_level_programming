4b1d09d83f497f68dddf5441e35fef9ddd0d635f
Albinnich <albina_haji@hotmail.com>
2024-06-07 09:51:58 +0000

    Code fixed

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,37 +1,48 @@
 import http.server
-import socketserver
 import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
-            self.send_header("Content-type", "text/plain")
+            self.send_header("Content-type", "text/html")
             self.end_headers()
-            self.wfile.write(b"Hello, this is your API response!")
-
+            self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == '/data':
-            data = {"name": "John", "age": 30, "city": "London"}
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
             self.wfile.write(json.dumps(data).encode())
 
-        elif self.path == '/info':
-            info = {"version": "1.0", "description": "A simple API
-                built with http.server"}
+        elif self.path == '/status':
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write(json.dumps(info).encode())
+            status = {
+                "status"L "OK"
+            }
+            self.wfile.write(json.dumps(status).encode())
 
         else:
             self.send_response(404)
-            self.send_header("Content-type", "text/plain")
+            self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write(b"Endpoint not found")
+            error_message = {
+                "error": "Endpoint not found"
+            }
+            self.wfile.write(json.dumps(error_message).encode())
 
-PORT = 8000
-with socketserver.TCPServer(("", PORT), MyRequestHandler) as httpd:
-    print(f"Serving on port {PORT}...")
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd server on port {port}')
     httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
