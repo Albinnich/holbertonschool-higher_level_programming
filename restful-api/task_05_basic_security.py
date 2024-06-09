a6c575f7553e4b86147ee2875642526f90840d65
Albinnich <albina_haji@hotmail.com>
2024-06-09 09:22:04 +0000

    Codes fixed

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""Module for secure API"""
+
 from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
