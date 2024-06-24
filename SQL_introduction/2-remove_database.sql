a842989d80153098951015e7830062f756a5e641
Albinnich <albina_haji@hotmail.com>
2024-06-24 10:43:43 +0000

    Another script added

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,4 +1,5 @@
 -- Script that deletes the database hbtn_0c_0 in my MySQL server
 -- If the database hbtn_0c_0 doesn't exist, my script should not fail
 
+DROP DATABASE IF EXISTS hbtn_0c_0;
 DROP DATABASE hbtn_0c_0;
