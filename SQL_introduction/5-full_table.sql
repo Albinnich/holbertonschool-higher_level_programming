99da9d57663862bf1fcb125eda530a0cc21a0b2f
Albinnich <albina_haji@hotmail.com>
2024-06-24 11:15:53 +0000

    Code added

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Sscript that prints the following description of the table first_table from the database hbtn_0c_0 in my MySQL server
+-- The database name will be passed as an argument of the mysql command
+
+{DESCRIBE | DESC} first_table;
