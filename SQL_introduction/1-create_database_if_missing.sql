acf4cc19198fe2b479cb06ebba288ec9bab9a874
Albinnich <albina_haji@hotmail.com>
2024-06-24 10:32:21 +0000

    Code fixed

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,5 @@
 -- Create the database hbtn_0c_0 in my MySQL server
 -- If the database hbtn_0c_0 already exists, my script should not fail
 
+DROP DATABASE IF EXISTS hbtn_0c_0;
 CREATE DATABASE hbtn_0c_0;
