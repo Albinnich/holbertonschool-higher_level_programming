2a6f1379701566d4e82960af5c291a87238455d2
Albinnich <albina_haji@hotmail.com>
2024-06-24 13:19:33 +0000

    code added

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Results should display both the score and the name (in this order)
+
+SELECT * FROM second_table
