1884ee35d9f0832e395e486be6dd64c1944971f0
Albinnich <albina_haji@hotmail.com>
2024-06-24 15:21:20 +0000

    some errors fixed

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,4 @@
 -- Script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server
 -- The database name will be passed as an argument of the mysql command
 
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.first_table
+SHOW CREATE TABLE first_table;
