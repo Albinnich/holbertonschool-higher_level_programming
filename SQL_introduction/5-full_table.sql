eb0e34ecd82f45122acf75e99dc635418555f1d2
Albinnich <albina_haji@hotmail.com>
2024-06-24 11:25:10 +0000

    Codes fixed

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,6 @@
--- Sscript that prints the following description of the table first_table from the database hbtn_0c_0 in my MySQL server
--- The database name will be passed as an argument of the mysql command
+-- Script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server
+-- he database name will be passed as an argument of the mysql command
 
-{DESCRIBE | DESC} first_table;
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM hbtn_0c_0
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'first_table';
