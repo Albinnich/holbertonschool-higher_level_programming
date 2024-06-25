86c1ef44ac656eb9bd18661c103c2861168a921a
Albinnich <albina_haji@hotmail.com>
2024-06-25 10:08:46 +0000

    Script added

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Script that creates the MySQL server user user_0d_1
+-- User_0d_1 should have all privileges on your MySQL server
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
