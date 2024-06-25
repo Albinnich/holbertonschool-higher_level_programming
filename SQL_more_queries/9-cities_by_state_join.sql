1594915adc2a31c7bf66d6ac6925dbeb8b6f5408
Albinnich <albina_haji@hotmail.com>
2024-06-25 13:21:00 +0000

    New script added

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,10 @@
+-- Script that lists all cities contained in the database hbtn_0d_usa.
+-- Each record should display: cities.id - cities.name - states.name
+
+SELECT id FROM cities;
+
+SELECT name FROM cities;
+
+SELECT name FROM states;
+
+ORDER BY id ASC;
